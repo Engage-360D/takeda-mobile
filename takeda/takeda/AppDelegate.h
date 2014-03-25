c74915442403d5f8892392fa1517ae7a68e93945
Serg <iSerg@Sergs-Mac-Pro.local>
2014-03-25 21:17:40 +0200

    create project

---
+++ takeda/takeda/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  takeda
+//
+//  Created by Serg on 3/25/14.
+//  Copyright (c) 2014 organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ takeda/takeda/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  takeda
+//
+//  Created by Serg on 3/25/14.
+//  Copyright (c) 2014 organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
